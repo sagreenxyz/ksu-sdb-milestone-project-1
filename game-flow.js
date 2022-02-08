9634d6230abdf74ff78b1294ec5b9fec55228168
Scott Green <sagreenxyz@gmail.com>
2022-02-08 14:18:31 -0600

    added control-panel

--- game-flow.js
+++ game-flow.js
@@ -10,17 +10,17 @@ function hideModalStartGame() {
     document.getElementById('modalStart').style.display = 'none';
 }
 
-function showModalComplete() {
+function showModalSuccess() {
     // shade the background
     document.querySelector('main').style.opacity = '0.3';
-    document.getElementById('modalComplete').style.display = 'block';
+    document.getElementById('modalSuccess').style.display = 'block';
 
 }
 
-function hideModalComplete() {
+function hideModalSuccess() {
     // unshade the background
     document.querySelector('main').style.opacity = '1.0';  // if transitioning to showModalStart, this line is not necessary
-    document.getElementById('modalComplete').style.display = 'none';
+    document.getElementById('modalSuccess').style.display = 'none';
 }
 
 function showModalTimeout() {
@@ -39,27 +39,27 @@ function hideModalTimeout() {
     document.getElementById('modalTimeout').style.display = 'none';
 }
 
-function showDivControl() {
-    document.getElementById('control').style.display = 'block';
+function showDivTimerControl() {
+    document.getElementById('timer-control').style.display = 'block';
     let startTime = Date.now();
     document.getElementById('timer').innerText = `Time Remaining: ${new Date(gameDurationLimitMS).toISOString().slice(14, 19)}`;
     clearTimeout(objTimer);
     objTimer = setInterval(() => {
         if ((Date.now() - startTime) >= gameDurationLimitMS) {
-            hideDivControl();
-            document.getElementById('control').classList.remove('blinking-warning-background')
+            hideDivtimer-control();
+            document.getElementById('timer-control').classList.remove('blinking-warning-background')
             showModalTimeout();
         }
         document.getElementById('timer').innerText = `Time Remaining: ${new Date(gameDurationLimitMS - (Date.now() - startTime)).toISOString().slice(14, 19)}`;
     }, 1000);
     setTimeout(() => {
         console.log('setTimeout')
-        document.getElementById('control').classList.add('blinking-warning-background')
+        document.getElementById('timer-control').classList.add('blinking-warning-background')
     }, warningStartTimeMS);
 }
 
-function hideDivControl() {
-    document.getElementById('control').style.display = 'none';
+function hideDivTimerControl() {
+    document.getElementById('timer-control').style.display = 'none';
 }
 
 function flowInitializeGame() {
@@ -95,4 +95,3 @@ function flowGameTimeout() {
         showModalStartGame();
     }, 10000);
 }
-
